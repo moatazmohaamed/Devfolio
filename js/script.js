059eac35405a0efb40278b5331a07e76cdfdd242
Moataz Mohamed <157921519+moatazmohaamed@users.noreply.github.com>
2024-10-23 16:03:12 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Typying animation
+
+var typed = new Typed(".auto-type", {
+    strings: ["Desginer", "Photographer", "Freelancer", "Developer"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true,
+});
+
+// Counter
+
+$(".count").counterUp({
+    delay: 10,
+    time: 2000,
+});
